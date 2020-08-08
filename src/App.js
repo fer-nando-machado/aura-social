2409da0da22b23faecc8e7c19cc24a2859ede3f8
Fernando Machado <lfmachado90@gmail.com>
2020-08-08 18:12:41 -0300

    Adds Instagram authorization

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import './App.css';
 
+function InstagramAccess({clientId, redirectUri}) {
+  const url = "https://api.instagram.com/oauth/authorize?" +
+  "client_id=" + process.env.REACT_APP_INSTAGRAM_CLIENT_ID +
+  "&redirect_uri=" + process.env.REACT_APP_URL +
+  "&scope=user_profile,user_media&response_type=code"
+
+  return (
+    <a href={url}>start</a>
+  );
+
+}
+
 function App() {
   return (
     <div className="App">
-      <span role="img" aria-label="crystal-ball">🔮</span>
+      <InstagramAccess/>
     </div>
   );
 }
