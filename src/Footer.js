c4f1bc3120bcc06932ec04317aa0e894e32c75f8
Fernando Machado <lfmachado90@gmail.com>
2020-08-12 21:10:10 -0300

    Updates home styles

--- src/Footer.js
+++ src/Footer.js
@@ -6,6 +6,8 @@ function Footer() {
     <div className="Footer">
       <a className="Link" href="https://github.com/fernandomachado90/aura-social">
         <span>Made with ♡ by Fernando Machado</span>
+      </a> | <a className="Link" href="https://www.flaticon.com/authors/good-ware">
+        <span>Icons by Good Ware</span>
       </a>
     </div>
   )
--- src/Header.js
+++ src/Header.js
@@ -4,10 +4,8 @@ import './Header.css';
 function Header({className}) {
   return (
     <div className={`Header ${className}`}>
-      <a href="/aura-social/">
-        <img src={process.env.REACT_APP_IMAGES + "lotus.svg"} alt="" />
-        <div className="HeaderTitle">Aura SociaL</div>
-      </a>
+      <img src={process.env.REACT_APP_IMAGES + "lotus.svg"} alt="" />
+      <div className="HeaderTitle">Aura SociaL</div>
     </div>
   )
 }
