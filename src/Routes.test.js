ea1908f945279b5b9e6bd45f34e099e9f6942dca
Fernando Machado <lfmachado90@gmail.com>
2020-08-15 14:43:19 -0300

    Asserts query params on Routes test

--- src/Routes.test.js
+++ src/Routes.test.js
@@ -2,19 +2,19 @@ import React from "react"
 import { render } from "@testing-library/react"
 import Routes from "./Routes"
 
-jest.mock('react-router-dom', () => ({
-  ...jest.requireActual('react-router-dom'),
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
   useLocation: () => ({
-    search: "?code=1234567890&error_description=this+is+an+error"
+    search: "?code=1234567890&error_description=this+is+an+error",
   }),
-}));
+}))
 
-jest.mock('./Home', () => ({query}) => <div className="Home" {...query} />)
+jest.mock("./Home", () => ({ query }) => <div className="Home" {...query} />)
 
 test("renders default Route with query params", () => {
   const { container, debug } = render(<Routes />)
   const home = container.querySelector(".Home")
   expect(home).toBeInTheDocument()
-  expect(home).toHaveAttribute('code', '1234567890')
-  expect(home).toHaveAttribute('error', 'this is an error')
+  expect(home).toHaveAttribute("code", "1234567890")
+  expect(home).toHaveAttribute("error", "this is an error")
 })
