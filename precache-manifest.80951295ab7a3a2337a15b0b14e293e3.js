cb279db9e2adb9d1471cc0a9c77cb6fa4193476b
Fernando Machado <lfmachado90@gmail.com>
2020-08-27 23:08:50 -0300

    Updates

---
+++ precache-manifest.80951295ab7a3a2337a15b0b14e293e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9e5b40790a0e69df1acc8850ea1a012",
+    "url": "/aura-social/index.html"
+  },
+  {
+    "revision": "b343869993e7e887600e",
+    "url": "/aura-social/static/css/main.caf6e7a8.chunk.css"
+  },
+  {
+    "revision": "008d50d177d5e783253c",
+    "url": "/aura-social/static/js/2.b16a4a1d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/aura-social/static/js/2.b16a4a1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b343869993e7e887600e",
+    "url": "/aura-social/static/js/main.2921c019.chunk.js"
+  },
+  {
+    "revision": "c349f0e33c20ec38d935",
+    "url": "/aura-social/static/js/runtime-main.c8f377a3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/aura-social/precache-manifest.399cab21c98d172802f8989058c1322f.js"
+  "/aura-social/precache-manifest.80951295ab7a3a2337a15b0b14e293e3.js"
 );
 
 self.addEventListener('message', (event) => {
