a4e0c872c5fb9cf2f8a60186004a28590f745d60
Fernando Machado <lfmachado90@gmail.com>
2020-09-03 14:37:48 -0300

    Includes period filter

--- src/components/InstagramMedia.js
+++ src/components/InstagramMedia.js
@@ -1,31 +1,64 @@
 import React, { useState, useEffect } from "react"
 
 import colors from "../external/colors"
-import debug from "../debug"
 
 import "./InstagramMedia.css"
 import "./InstagramRays.scss"
 
 function InstagramMedia({ media }) {
-  const [index, setIndex] = useState(0) //useState(9)
+  const [settings, setSettings] = useState()
+  const [target, setTarget] = useState()
+  const [dateFrom, setDateFrom] = useState()
+  const [dateTo, setDateTo] = useState()
+
+  const [index, setIndex] = useState(0)
   const [palette, setPalette] = useState([])
   const [aura, setAura] = useState()
-
-  const username = media.username
-  const images = media.images
-  const total = images.length
-  const inProgress = index < total
-  const progress = Math.floor((index * 100) / total)
 
   async function fetchColor(img) {
     const rgb = colors.getPalette(img, 1)
     setPalette(palette.concat(rgb))
-    //setPalette(debug.paletteRandomizer(100))
-    setIndex(index + 1)
+    if (index < target.total) {
+      setIndex(index + 1)
+    }
   }
 
   useEffect(() => {
-    if (inProgress) return
+    const min = dateToString(media.images[0].date)
+    const max = dateToString(media.images[media.images.length - 1].date)
+
+    setSettings({
+      min: min,
+      max: max,
+    })
+    setDateFrom(min)
+    setDateTo(max)
+  }, [media])
+
+  useEffect(() => {
+    if (!dateFrom || !dateTo) return
+
+    const from = new Date(dateFrom)
+    const to = new Date(dateTo)
+    to.setDate(to.getDate() + 1)
+
+    const images = media.images.filter((img) => {
+      return from <= img.date && img.date <= to
+    })
+
+    const total = images.length
+    setTarget({
+      images: images,
+      total: total,
+    })
+
+    setIndex(0)
+    setPalette([])
+    setAura(null)
+  }, [media, dateFrom, dateTo])
+
+  useEffect(() => {
+    if (!target || !index || !palette || !target.total || index < target.total) return
 
     const gradient = palette
       .map((rgb) => ({ rgb: rgb, hsl: colors.rgb2hsl(rgb) }))
@@ -38,19 +71,56 @@ function InstagramMedia({ media }) {
     })
 
     setAura(`url(${conicGradient.dataURL})`)
-  }, [palette, inProgress])
+  }, [target, index, palette])
+
+  if (!target) return <div className="InstagramMedia Content" />
+
+  const complete = index === target.total
+  const progress = Math.floor((index * 100) / target.total) + "%"
 
   return (
     <div className="InstagramMedia Content">
-      {inProgress && (
-        <>
-          <img crossOrigin="anonymous" alt="" src={images[index].url} onLoad={(event) => fetchColor(event.target)} />
-          <span>{progress}%</span>
-        </>
-      )}
-      {!inProgress && <div className="aura rays" style={{ backgroundImage: aura }} />}@{username}
+      <div className="InstagramFilter">
+        <p>Select the period you wish to scan an aura from:</p>
+        <input
+          type="date"
+          value={dateFrom}
+          min={settings.min}
+          max={dateTo}
+          onChange={(e) => (e.target.value > dateTo ? setDateFrom(dateTo) : setDateFrom(e.target.value))}
+        />
+        {" to "}
+        <input
+          type="date"
+          value={dateTo}
+          min={dateFrom}
+          max={settings.max}
+          onChange={(e) => (e.target.value < dateFrom ? setDateTo(dateFrom) : setDateTo(e.target.value))}
+        />
+      </div>
+
+      <div className="InstagramAura">
+        {!complete && (
+          <>
+            <img
+              crossOrigin="anonymous"
+              alt=""
+              src={target.images[index].url}
+              onLoad={(event) => fetchColor(event.target)}
+            />
+            <span>{progress}</span>
+          </>
+        )}
+        {complete && <div className={`aura ${aura ? "rays" : ""}`} style={{ backgroundImage: aura }} />}
+      </div>
+
+      <div className="InstagramShare">@{media.username}</div>
     </div>
   )
+}
+
+function dateToString(date) {
+  return date.toISOString().slice(0, 10)
 }
 
 export default InstagramMedia
